d0de09c544ea8f86f8a3537338a8ad5f69de2576
LeeAnnJ <lee_ann.j@qq.com>
2022-11-14 20:31:55 +0800

    init frontend project

---
+++ src/pages/jump/jump.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>成功跳转界面！</p>
+		<p>{{count}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				count:0
+			}
+		},
+		mounted() {
+			console.log("in jump!");
+		}
+	}
+</script>
+
+<style>
+</style>
