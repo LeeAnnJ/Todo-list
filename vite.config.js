9116c5851b74021fc1d8664478c3b3989bd2bd16
LeeAnnJ <lee_ann.j@qq.com>
2022-11-16 09:11:31 +0800

    renamed:    node_modules/resolve/.eslintignore -> .gitignore 	modified:   index.html 	deleted:    node_modules/.bin/nanoid 	deleted:    node_modules/.bin/nanoid.cmd 	deleted:    node_modules/.bin/nanoid.ps1 	modified:   node_modules/.vite/_metadata.json 	deleted:    node_modules/.vite/chunk-MEOA3D7T.js 	deleted:    node_modules/.vite/chunk-MEOA3D7T.js.map 	modified:   node_modules/.vite/vue-router.js 	modified:   node_modules/.vite/vue-router.js.map 	modified:   node_modules/.vite/vue.js 	modified:   node_modules/@babel/parser/lib/index.js 	modified:   node_modules/@babel/parser/lib/index.js.map 	modified:   node_modules/@babel/parser/package.json 	modified:   node_modules/@babel/parser/typings/babel-parser.d.ts 	deleted:    node_modules/is-core-module/.eslintignore 	modified:   node_modules/is-core-module/.eslintrc 	modified:   node_modules/is-core-module/CHANGELOG.md 	modified:   node_modules/is-core-module/core.json 	modified:   node_modules/is-core-module/package.json 	modified:   node_modules/is-core-module/test/index.js 	modified:   node_modules/resolve/.eslintrc 	deleted:    node_modules/resolve/appveyor.yml 	modified:   node_modules/resolve/lib/async.js 	modified:   node_modules/resolve/lib/core.js 	modified:   node_modules/resolve/lib/core.json 	modified:   node_modules/resolve/lib/node-modules-paths.js 	modified:   node_modules/resolve/lib/sync.js 	modified:   node_modules/resolve/package.json 	modified:   node_modules/resolve/readme.markdown 	deleted:    node_modules/resolve/test/.eslintrc 	modified:   node_modules/resolve/test/core.js 	modified:   node_modules/resolve/test/mock_sync.js 	modified:   node_modules/resolve/test/node_path.js 	modified:   node_modules/resolve/test/resolver.js 	modified:   node_modules/resolve/test/resolver/invalid_main/package.json 	modified:   node_modules/resolve/test/resolver_sync.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js 	modified:   src/pages/hello/HelloWorld.vue 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite' // 配置ElementPlus按需引入
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ]
 })
