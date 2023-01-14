b0832ccdc19713b8b39b799ffa0f9e19a147ef03
yangyf83 <yangyf83@aliyun.com>
2023-01-14 21:54:02 +0800

    update avator dir

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'static')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 // allow cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // allow cors
 app.use((req, res, next) => {
